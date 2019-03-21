5809770a3b79211173f5cca64ce43da651aced65
oscarkaranim <oscarkaranim@gmail.com>
2019-03-21 12:17:44 +0300

    updated settings, created app, added south, enabled django admin, boom

--- my_django16_project/my_django16_project/urls.py
+++ my_django16_project/my_django16_project/urls.py
@@ -4,9 +4,9 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'my_django16_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+     # Examples:
+     url(r'^$', 'my_django16_project.views.home', name='home'),
+     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
